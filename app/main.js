7cb040dc99225531d944baada4830b0c6da1f9dd
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-10-06 23:14:40 +0300

    pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -4,6 +4,17 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
+const createResponse = (info) => {
+    return "HTTP/1.1 " + info + " \r\n\r\n";
+}
+const handleRequest = (data) => {
+    const [info, _] = data.split("\r\n");
+    const [__, path] = info.split(" ");
+    if(path==="/"){
+        return "200 OK";
+    }
+    return "404 Not Found";
+}
 const server = net.createServer((socket) => {
   console.log("SERVER: Starting");  
   socket.on("close", () => {
@@ -11,7 +22,9 @@ const server = net.createServer((socket) => {
     server.close();
   });
   socket.on("data", (data) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    const info = handleRequest(data.toString());
+    const res = createResponse(info);
+    socket.write(res);
     socket.end();
   });
   socket.on("end", () => {
