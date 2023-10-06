ea7a2e3c6811b801d9e972d425e0a47167d8a497
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-10-06 22:56:06 +0300

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -5,9 +5,18 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  console.log("SERVER: Starting");  
   socket.on("close", () => {
     socket.end();
     server.close();
+  });
+  socket.on("data", (data) => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    socket.end();
+  });
+  socket.on("end", () => {
+    socket.end();
+    console.log("SERVER: socket end");    
   });
 });
 
