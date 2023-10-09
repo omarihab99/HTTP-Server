589f4e8e1b3b5e163c8741c5d532304ae55b05ba
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-10-09 19:20:42 +0300

    pass 7th stage

--- app/main.js
+++ app/main.js
@@ -11,7 +11,6 @@ const STATUS_CODES = {
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-
 const createResponse = ({method, path, version, headers}) => {
   if(path === "/") {
     return `${version} 200 OK\r\n\r\n`;
