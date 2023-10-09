b42fd8e3e7c04c2330fe7582f7d0574df2cfec21
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-10-09 19:30:54 +0300

    pass 7th stage

--- app/readfile.js
+++ app/readfile.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 
 function getFileContent (filePath){
+    if(!fs.existsSync(filePath)) {
+        return null;
+    }
     return fs.readFileSync(filePath, "utf8");
 }
 module.exports = getFileContent;
